0346f979782c9dadd0253867a288291c0db10e37
GillianY1 <gilliangilliany@gmail.com>
2020-09-28 23:13:01 +0800

    add 5 image object in an array , show index with ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pages';
+  images = [
+    {
+      title : 'Milano',
+      url:'https://images.unsplash.com/photo-1543864080-6d6644ddcbd6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+    },
+    {
+      title :'Bergamo',
+      url:'https://images.unsplash.com/photo-1597425984755-3cd06242b013?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60'
+    },
+    {
+      title: 'Venice',
+      url:'https://images.unsplash.com/photo-1514890547357-a9ee288728e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60'
+    },
+    {
+      title:'Roma',
+      url :'https://images.unsplash.com/photo-1569230516306-5a8cb5586399?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60'
+    },
+    {
+      title:'Firenze',
+      url :'https://images.unsplash.com/photo-1574070379047-766d93675fa1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60'
+    },
+    {
+      title:'Vatican',
+      url:'https://images.unsplash.com/photo-1531572753322-ad063cecc140?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60'
+    } 
+  ];
 }
